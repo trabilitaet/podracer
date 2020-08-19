80547b521025c3588a2e80fb953862cc79a9f4bb
Silvia <silvia.ritsch@scs.ch>
2020-08-19 08:28:54 +0200

    fix logging in PID

--- pygamecsb/controller_PID.py
+++ pygamecsb/controller_PID.py
@@ -16,25 +16,25 @@ class PID():
         self.los_error = 0
         self.cum_los_error = 0
         self.old_los_error = 0
-        log('initialized PID with:')
-        log('Kp,Ki,Kd: ')
-        log(Kp)
-        log(Ki)
-        log(Kd)
+        self.log('initialized PID with:')
+        self.log('Kp,Ki,Kd: ')
+        self.log(self.Kp)
+        self.log(self.Ki)
+        self.log(self.Kd)
 
 
-    def calculate(self, x, y, vx, vy, next_checkpoint_x, next_checkpoint_y, next_checkpoint_angle):
+    def calculate(self, x, y, theta, vx, vy, next_checkpoint_x, next_checkpoint_y, next_checkpoint_angle):
         self.time += 1
 
-        log(next_checkpoint_angle)
+        self.log(next_checkpoint_angle)
         dist = np.linalg.norm(np.array([x,y])-np.array([next_checkpoint_x,next_checkpoint_y]))
         self.old_los_error = self.los_error
         self.los_error = math.cos(next_checkpoint_angle)*dist
-        log(self.los_error)
+        self.log(self.los_error)
         self.cum_los_error += self.los_error * self.time
-        log(self.cum_los_error)
+        self.log(self.cum_los_error)
         self.rate_los_error = (self.los_error - self.old_los_error) / self.time
-        log(self.rate_los_error)
+        self.log(self.rate_los_error)
 
         pid = self.Kp * self.los_error + self.Ki * self.cum_los_error + self.Kd * self.rate_los_error
         thrust = int(pid)
@@ -45,10 +45,10 @@ class PID():
         return thrust, next_checkpoint_x, next_checkpoint_y
 
     def log(self, message):
-        filename = 'log_' + self.get_name()
-        logfile =  open(filename, 'w')
-        logfile.writelines(str(message) + '\n')
-        logfile.close()      
+        filename = 'self.log_' + self.get_name()
+        self.logfile =  open(filename, 'w')
+        self.logfile.writelines(str(message) + '\n')
+        self.logfile.close()
 
     def get_name(self):
         return 'PID'
